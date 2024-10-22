4d06c3479b00f66fcf7f8beee4d270767b17aecc
hanqihua01 <qihua.han@icloud.com>
2024-10-22 15:06:52 +0200

    recover

--- memory_bound/ppm.py
+++ memory_bound/ppm.py
@@ -1,27 +1,19 @@
 import numpy as np
 import time
 import pp
-from multiprocessing import Array
-
-matrix_size = 4800
 
 def memory_bound_task(start_row, end_row, matrix):
-    matrix_np = np.frombuffer(matrix.get_obj()).reshape((matrix_size, matrix_size))
-    rows, cols = matrix_np.shape
-
+    rows, cols = matrix.shape
     for i in range(start_row, end_row):
         for j in range(cols):
-            matrix_np[i][j] *= 1.01
+            matrix[i][j] *= 1.01
     return
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    matrix = Array('d', matrix_size * matrix_size)
-    matrix_np = np.frombuffer(matrix.get_obj()).reshape((matrix_size, matrix_size))
-
-    np.random.seed(0)
-    matrix_np[:] = np.random.rand(matrix_size, matrix_size)
+    matrix_size = 4800
+    matrix = np.random.rand(matrix_size, matrix_size)
 
     job_server = pp.Server()
 
