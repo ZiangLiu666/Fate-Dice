378a77c0007633aa41745c39f6985692b8537341
hanqihua01 <qihua.han@icloud.com>
2024-10-24 15:34:14 +0200

    m

--- io_bound/multithreads.py
+++ io_bound/multithreads.py
@@ -2,12 +2,13 @@ import threading
 import time
 
 def read_and_write_file(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-        file.close()
-    with open(file_path + '.out.txt', 'w') as file:
-        file.write(content)
-        file.close()
+    for i in range(100):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            file.close()
+        with open(file_path, 'w') as file:
+            file.write(content)
+            file.close()
 
 file_paths = ['test0.txt', 'test1.txt', 'test2.txt', 'test3.txt']
 
